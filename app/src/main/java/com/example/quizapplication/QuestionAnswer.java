5cf419c3c89f875c3b81084c41311ff6a20052bb
Charu <charut626@gmail.com>
2023-08-19 18:22:27 +0530

    Quiz

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which company own the android?",
+            "Which one is not the programming language?",
+            "What is the size of int data type?",
+            "Who is the CEO of Google?"
+    };
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"1 byte","2 bytes","3 bytes","4 bytes"},
+            {"Satya Nadela","Sundar Pichai","Elon Musk","Mark Zuckerberg"}
+    };
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "4 bytes",
+            "Sundar Pichai"
+    };
+}
